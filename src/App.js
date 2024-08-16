0c0a5656a05505164bcf87045b2111a31335e658
Hyun <jjjtoy@naver.com>
2024-08-16 09:29:20 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/test")
+      .then((response) => {
+        setData(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error("에러 ", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>API Data</h1>
+          {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>로딩</p>}
+        </div>
       </header>
     </div>
   );
