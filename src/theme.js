1709ca9928f518fe88d30ac6cc3135da92b6d9d0
Hyun <jjjtoy@naver.com>
2024-08-19 13:17:08 +0900

    feat: theme.js(컬러) 세팅

---
+++ src/theme.js
@@ -0,0 +1,48 @@
+const color = {
+  primary: "#FE6F6F",
+  button: {
+    blue: "#4E87E9",
+    primary: "#FE6F6F",
+  },
+  timeGrid: {
+    20: "#FAE7C4",
+    40: "#FFDA95",
+    40: "#FFB062",
+    40: "#FF6F6F",
+    40: "#FD2734",
+    blur: "#D7D7D7",
+    line: "#868686",
+    selected: "#FE6F6F",
+    select: "#FFA29D",
+  },
+};
+const text = {
+  primary: "#0D0D0D",
+  gamma: {
+    900: "#F2F2F2",
+    800: "#D7D7D7",
+    500: "#868686",
+  },
+};
+const styles = {
+  flexCenterRow: `
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    `,
+  flexCenterColumn: `
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    `,
+};
+
+const theme = {
+  color,
+  styles,
+  text,
+};
+
+export default theme;
