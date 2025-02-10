9c5ea52080359acd6e7a04237f88e0f4746429bc
Hyun <jjjtoy@naver.com>
2025-02-10 15:40:27 +0900

    fix: 페이지 별 seo 적용을 위한 id 추가가

--- src/Seo.js
+++ src/Seo.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import React, { useEffect } from "react";
 
 const Seo = ({
   title = "타임테이블",
   description = "소,대규모 모임 - 인원 별 시간 정리",
   url = "https://www.timetable2.com/",
-}) => (
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:url" content={url} />
-    {/* {image && <meta property="og:image" content={image} />} */}
-  </Helmet>
-);
+}) => {
+  useEffect(() => {
+    if (title) document.querySelector('meta[property="og:title"]').setAttribute("content", title);
+    if (description)
+      document
+        .querySelector('meta[property="og:description"]')
+        .setAttribute("content", description);
+    if (url) document.querySelector('meta[property="og:url"]').setAttribute("content", url);
+    // if (image) document.querySelector('meta[property="og:image"]').setAttribute("content", image);
+  }, [title, description, url]);
+
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </>
+  );
+};
 
 export default Seo;
